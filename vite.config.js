65c22262215c38495d6f8a6f9dfca21f35d73b80
D Mohan sai kumar <dmohansaikumar@gmail.com>
2025-02-10 10:48:00 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,7 @@
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Green Fusion',
-        short_name: 'Green Fusion',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: '/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
-});
+  plugins: [react()],
+})
